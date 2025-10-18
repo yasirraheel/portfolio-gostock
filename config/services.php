ba06a5545be5508f196058685a7e9607a7bf4910
Muhammad Asif Shabbir <islammujahid921@gmail.com>
2025-10-18 14:05:02 +0500

    Revert oauth callback construction to env('APP_URL').'/oauth/...' per console update

--- config/services.php
+++ config/services.php
@@ -39,19 +39,19 @@ return [
     'facebook' => [
       'client_id' => env('FACEBOOK_CLIENT_ID'),
       'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-      'redirect' => rtrim(env('APP_URL'), '/') . '/oauth/facebook/callback',
+      'redirect' => env('APP_URL').'/oauth/facebook/callback',
       ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => rtrim(env('APP_URL'), '/') . '/oauth/google/callback',
+        'redirect' => env('APP_URL').'/oauth/google/callback',
     ],
 
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => rtrim(env('APP_URL'), '/') . '/oauth/twitter/callback',
+        'redirect' => env('APP_URL').'/oauth/twitter/callback',
     ],
 
 ];
